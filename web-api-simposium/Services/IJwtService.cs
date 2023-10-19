8d3ecb8e5b4aad3e6e8b0f632177f0ebb5ae0620
SebastianTenorio <jaime.tenorio@welldex.com>
2023-10-19 01:02:47 -0500

    creacion del servicio para generar los tokens y creacion de funcion para crear el token

---
+++ web-api-simposium/Services/IJwtService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using web_api_simposium.Models.Authentications;
+
+namespace web_api_simposium.Services
+{
+    public interface IJwtService
+    {
+        SecurityToken GenerateToken(TokenData tokenData, string secretKeyId);
+    }
+}
---
+++ web-api-simposium/Services/JwtService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using web_api_simposium.Models.Authentications;
+
+namespace web_api_simposium.Services
+{
+    public class JwtService : IJwtService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public SecurityToken GenerateToken(TokenData tokenData, string secretKeyId)
+        {
+            var authClaims = new List<Claim>
+            {
+               new Claim("userId", tokenData.UserId)
+            };
+
+            var key = Encoding.ASCII.GetBytes(_configuration[$"authenticationSettings:jwt:{secretKeyId}"]);
+
+            var token = new JwtSecurityTokenHandler().CreateToken(new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(authClaims),
+                Expires = DateTime.UtcNow.AddMinutes(5),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                IssuedAt = DateTime.UtcNow
+            });
+
+            return token;
+        }
+    }
+}
