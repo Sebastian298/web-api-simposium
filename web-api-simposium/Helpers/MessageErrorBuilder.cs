aff689b374b97e8cafd5386d7d8abf70a280691f
SebastianTenorio <jaime.tenorio@welldex.com>
2023-10-19 00:03:05 -0500

    creacion de clase para generar la estructura de error de exepcion

---
+++ web-api-simposium/Helpers/MessageErrorBuilder.cs
@@ -0,0 +1,18 @@
+﻿using web_api_simposium.Models.Responses;
+
+namespace web_api_simposium.Helpers
+{
+    public static class MessageErrorBuilder
+    {
+        public static GenericResponseData GenerateError(string innerException)
+        {
+            return new GenericResponseData
+            {
+                Type = "danger",
+                Title = "Error",
+                Message = "Error inesperado, intente de nuevo o contacte a soporte",
+                InnerException = innerException
+            };
+        }
+    }
+}
