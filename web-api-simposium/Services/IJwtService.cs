203d984973f7023540d7a543173a98456dec381b
SebastianTenorio <jaime.tenorio@welldex.com>
2023-10-19 01:17:13 -0500

    funcion para validar el token enviado y para refrescar el token si este ya se a vencido terminado

--- web-api-simposium/Services/IJwtService.cs
+++ web-api-simposium/Services/IJwtService.cs
@@ -6,5 +6,6 @@ namespace web_api_simposium.Services
     public interface IJwtService
     {
         SecurityToken GenerateToken(TokenData tokenData, string secretKeyId);
+        TokenData ValidateToken(string token, string secretKeyId);
     }
 }
