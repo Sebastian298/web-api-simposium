707d0a47af10f79c1b578e56d8c0363295b447ff
SebastianTenorio <jaime.tenorio@welldex.com>
2023-10-19 00:52:45 -0500

    Creacion de modelo para el servicio de jwt

---
+++ web-api-simposium/Models/Authentications/TokenData.cs
@@ -0,0 +1,11 @@
+﻿namespace web_api_simposium.Models.Authentications
+{
+    public class TokenData
+    {
+        public bool HasError { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public string InnerException { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+    }
+}
