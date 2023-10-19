871b5895d8f3b5b47c7c3d77ee434b60c5883c6f
SebastianTenorio <jaime.tenorio@welldex.com>
2023-10-19 00:13:41 -0500

    Creacion del context de dapper

---
+++ web-api-simposium/Data/DapperContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace web_api_simposium.Data
+{
+    public class DapperContext
+    {
+        private readonly IConfiguration _configuration;
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IDbConnection CreateConnection(string connectionName) => new SqlConnection(_configuration[$"connectionStrings:{connectionName}"]);
+    }
+}
